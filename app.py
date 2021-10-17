821e06c8d45ab0c76cd95710d81a2f3cd31e1ea0
FredGalway <fmoitry@gmail.com>
2021-10-17 15:52:59 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,51 @@
+import tkinter as tk
+import PyPDF2
+from PIL import Image, ImageTk
+from tkinter.filedialog import askopenfile
+
+root = tk.Tk()
+root.title("PDF text extractor")
+canvas = tk.Canvas(root, width=600, height=300)
+canvas.grid(columnspan=3, rowspan=3)
+
+# Logo
+logo = Image.open('img/logo.png')
+logo = ImageTk.PhotoImage(logo)
+
+logo_label = tk.Label(image = logo)
+logo_label.image = logo
+logo_label.grid(column = 1, row = 0)
+
+# Instructions
+instruction = tk.Label(root, text="Select a PDF file on your computer to extract all its text", font="Raleway")
+instruction.grid(columnspan=3, column=0, row=1)
+
+def open_file():
+    browse_text.set("loading ...")
+    pdf_file_opened = askopenfile(parent=root, mode='rb', title="Choose a PDF file", filetypes=[("PDF Files", "*.pdf")])
+    if pdf_file_opened:
+        read_pdf = PyPDF2.PdfFileReader(pdf_file_opened)
+        page = read_pdf.getPage(0)
+        page_content = page.extractText()
+        #print(page_content)
+
+        # Text box
+        text_box = tk.Text(root, height=10, width=50, padx=15, pady=15)
+        text_box.insert(1.0, page_content)
+        text_box.tag_configure("center", justify="center")
+        text_box.tag_add("center", 1.0, "end")
+        text_box.grid(column=1, row=3)
+
+        # button text label
+        browse_text.set("Browse")
+
+# browse button
+browse_text = tk.StringVar()
+browse_btn = tk.Button(root, textvariable=browse_text, font="Raleway", bg="#20bebe", fg="#20bebe", height=2, width=15, command=open_file)
+browse_text.set("Browse")
+browse_btn.grid(column=1, row=2)
+
+canvas = tk.Canvas(root, width=600, height=250)
+canvas.grid(columnspan=3)
+
+root.mainloop()
